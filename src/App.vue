b899dbb713ba0ce6e0430d8705666ef652e2d23a
pvakili <pooriavakili>
2021-04-07 15:34:17 +0430

    firstcommit:

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Ant Design Vue 3 in CodeSandbox!" />
+  <a-button type="primary" @click="open"> go antdv.com </a-button>
+  <div>
+  <a-radio >Radio</a-radio>
+  </div>
+</template>
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  setup() {
+    return {
+      open: () => {
+        window.open("https://antdv.com", "_blank");
+      },
+    };
+  },
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello ">
+    <a-button class=" text-2xl text-white bg-black"  >Primary</a-button>
+    <a-button ghost>Default</a-button>
+    <a-button type="dashed" ghost>Dashed</a-button>
+    <a-button type="danger" ghost>Danger</a-button>
+    <a-button type="link" ghost>Link</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
